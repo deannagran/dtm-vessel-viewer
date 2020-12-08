f71bad01b9652bccffe5a1436e46a91fe63e3b46
jose.jon <josejon@users.noreply.github.com>
2020-12-07 20:11:49 -0500

    Update Register.js

--- client/src/views/auth/Register.js
+++ client/src/views/auth/Register.js
@@ -41,67 +41,57 @@ export default function Register() {
   };
 
   return (
-    <div id ="root">
-	<div className="limiter">
-		<div className="container-login100">
-			<div className="wrap-login100">
+    <div>
+	    <h2>Register</h2>
       {error && (
         <ErrorNotice message={error} clearError={() => setError(undefined)} />
       )}
-				<form className="login100-form validate-form p-l-55 p-r-55 p-t-178" onSubmit={submit}>
-					<span className="login100-form-title">
-						Register
-					</span>
+        <form className="form" onSubmit={submit}>
+          <label htmlFor="login-email">First Name</label>
+          <input
+            id="login-email"
+            type="text"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
 
-					<div className="wrap-input100 validate-input m-b-16" data-validate="Please enter First name">
-						<input className="input100" type="text" name="fname" placeholder="First name*" onChange={(e) => setFirstName(e.target.value)}/>
-						<span className="focus-input100"></span>
-					</div>
+          <label htmlFor="login-email">Last Name</label>
+          <input
+            id="login-email"
+            type="text"
+            onChange={(e) => setLastName(e.target.value)}
+          />
 
-					<div className="wrap-input100 validate-input" data-validate = "Please enter Last name">
-						<input className="input100" type="text" name="lname" placeholder="Last Name*" onChange={(e) => setLastName(e.target.value)}/>
-						<span className="focus-input100"></span>
-					</div>
+          <label htmlFor="login-email">Company Name</label>
+          <input
+            id="login-email"
+            type="text"
+            onChange={(e) => setCompanyName(e.target.value)}
+          />
 
-          <div className="wrap-input100 validate-input" data-validate = "Please enter Company name">
-						<input className="input100" type="text" name="cname" placeholder="Company Name" onChange={(e) => setCompanyName(e.target.value)}/>
-						<span className="focus-input100"></span>
-					</div>
+          <label htmlFor="login-email">Email</label>
+          <input
+            id="login-email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-          <div className="wrap-input100 validate-input" data-validate = "Please enter Email Address">
-						<input className="input100" type="text" name="email" placeholder="Email*" onChange={(e) => setEmail(e.target.value)}/>
-						<span className="focus-input100"></span>
-					</div>
+          <label htmlFor="login-password">Password</label>
+          <input
+            id="login-password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <div className="wrap-input100 validate-input" data-validate = "Please enter password">
-						<input className="input100" type="password" name="pass" placeholder="Password*" onChange={(e) => setPassword(e.target.value)}/>
-						<span className="focus-input100"></span>
-					</div>
+          <label htmlFor="login-password">Verify Password</label>
+          <input
+            id="login-password"
+            type="password"
+            onChange={(e) => setPasswordCheck(e.target.value)}
+          />
 
-          <div className="wrap-input100 validate-input" data-validate = "Please reenter password">
-						<input className="input100" type="password" name="pass" placeholder="Verify Password" onChange={(e) => setPasswordCheck(e.target.value)}/>
-						<span className="focus-input100"></span>
-					</div>
-
-          <div className="container-login100-form-btn">
-						<button className="login100-form-btn" onClick={submit}>
-							Register
-						</button>
-					</div>
-
-					<div className="flex-col-c p-t-90 p-b-30">
-						<span className="txt1 p-b-9">
-							Already have an account?
-						</span>
-
-						<a href="/Login" className="txt3">
-							Login
-						</a>
-					</div>
-				</form>
-			</div>
+          <input type="submit" value="Register" />
+          
+        </form>
 		</div>
-	</div>
-  </div>
   );
 }
